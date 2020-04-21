2c5201ed66e7b73592423092eb50bbb1a0ceaf43
Wolney Carneiro <wolneycarneiro91@gmail.com>
2020-04-21 03:23:32 -0300

    url MONGO_URI

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV =="production"){
-    module.exports = {mongoURI: "mongodb+srv://userw:<Senhas7775965>@cluster0-n1qzq.mongodb.net/test?retryWrites=true&w=majority"}
+    module.exports = {mongoURI: "mongodb://userw:Senhas7775965@cluster0-shard-00-00-n1qzq.mongodb.net:27017,cluster0-shard-00-01-n1qzq.mongodb.net:27017,cluster0-shard-00-02-n1qzq.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"}
 }else{    
     module.exports = {mongoURI: "mongodb://localhost/blogapp"}
 
