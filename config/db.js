01fb686a390160bbfd575f166a5a1afa9e228b4e
wolneycarneiro91 <46429367+wolneycarneiro91@users.noreply.github.com>
2020-04-21 11:07:11 -0300

    Delete db.js
    credenciais

--- config/db.js
+++
@@ -1,6 +0,0 @@
-if(process.env.NODE_ENV =="production"){
-    module.exports = {mongoURI: "mongodb+srv://userw:Senhas7775965@cluster0-n1qzq.mongodb.net/test?retryWrites=true&w=majority"}
-}else{    
-    module.exports = {mongoURI: "mongodb://localhost/blogapp"}
-
-}
